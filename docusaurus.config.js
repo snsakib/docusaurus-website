a31eba763a873bb4b4c546f506e189694474344f
snsakib <contact@snsakib.com>
2020-12-16 03:23:48 +0600

    change header & footer content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,48 +23,11 @@ module.exports = {
         {to: 'educations', label: 'Educations', position: 'left'},
         {to: 'about', label: 'About', position: 'left'},
         {to: 'contact', label: 'Contact', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Syed Nazmus Sakib.`,
     },
   },
   presets: [
