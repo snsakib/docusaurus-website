edb43a4f9c553585383ef9de8703ceb444cc06e6
snsakib <contact@snsakib.com>
2020-12-16 03:09:55 +0600

    change footer content

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -55,40 +55,6 @@ function Footer(): JSX.Element | null {
         'footer--dark': footer.style === 'dark',
       })}>
       <div className="container">
-        {links && links.length > 0 && (
-          <div className="row footer__links">
-            {links.map((linkItem, i) => (
-              <div key={i} className="col footer__col">
-                {linkItem.title != null ? (
-                  <h4 className="footer__title">{linkItem.title}</h4>
-                ) : null}
-                {linkItem.items != null &&
-                Array.isArray(linkItem.items) &&
-                linkItem.items.length > 0 ? (
-                  <ul className="footer__items">
-                    {linkItem.items.map((item, key) =>
-                      item.html ? (
-                        <li
-                          key={key}
-                          className="footer__item"
-                          // Developer provided the HTML, so assume it's safe.
-                          // eslint-disable-next-line react/no-danger
-                          dangerouslySetInnerHTML={{
-                            __html: item.html,
-                          }}
-                        />
-                      ) : (
-                        <li key={item.href || item.to} className="footer__item">
-                          <FooterLink {...item} />
-                        </li>
-                      ),
-                    )}
-                  </ul>
-                ) : null}
-              </div>
-            ))}
-          </div>
-        )}
         {(logo || copyright) && (
           <div className="text--center">
             {logo && logo.src && (
