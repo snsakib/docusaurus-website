4845cf6691fcf223cfed604ecb916059ba41bdda
snsakib <contact@snsakib.com>
2020-12-16 06:09:11 +0600

    ignore broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ module.exports = {
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
